54527557081978794e0d81c1fb80f209a97603e4
Leonardo Klinger <gamefivebr@gmail.com>
2022-05-11 16:05:52 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.svg'
+import "./style.css"
+
+function App() {
+  const reserveSize = useSelector(state => state.reserve.length)
+
+  return (
+    <header className='container'>
+      <Link to="/">
+        <img className='logo' src={logo} alt="Logo Projeto" />
+      </Link>
+
+      <Link className='reserva' to="/reservas">
+        <div>
+          <strong>Minhas reservas</strong>
+          <span>{reserveSize} reservas</span>
+        </div>
+      </Link>
+    </header>
+  );
+}
+
+export default App;
